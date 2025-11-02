8b7b0f675040e00f820d2408ffabbd0b94d3fe0a
Santiago <santiagoinfante2010@gmail.com>
2025-11-02 18:02:35 -0400

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "MystiaAi está viva 💖"
+
+def keep_alive():
+    t = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
+    t.start()
